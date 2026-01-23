4feeff4895a144a4c580cd41cbe71e2e2f246ae1
Rvs Tharun <tharunrompilli15@gmail.com>
2026-01-23 22:41:58 +0530

    Challenge-05

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+import styles from './Layout.module.css';
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.layout}>
+      <header className={styles.header}>
+        <NavBar />
+      </header>
+
+      <main className={styles.main}>
+        {children}
+      </main>
+
+      <footer className={styles.footer}>
+        <p>&copy; 2026 BlogApp. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
